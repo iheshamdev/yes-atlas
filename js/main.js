88b88aaaba289201b53e8dd8a3da9c557f848df4
Hesham <heshamabdulzaher@gmail.com>
2021-11-14 10:30:35 +0200

    refactor(modal) : toggle modal functionality

--- js/main.js
+++ js/main.js
@@ -1,7 +1,21 @@
 // toggle addresses panels
-const togglers = document.querySelectorAll(".address__header");
+const togglers = document.querySelectorAll('.address__header');
 togglers.forEach(itm => {
-    itm.addEventListener('click', (e) => {
-        e.target.closest('.address').classList.toggle('expand');
-    });
+	itm.addEventListener('click', e => {
+		e.target.closest('.address').classList.toggle('expand');
+	});
 });
+
+// Show & close modal
+const modal = document.querySelector('.modal');
+const sendRequestsBtns = document.querySelectorAll(
+	'#destinations .card .btn-primary'
+);
+sendRequestsBtns.forEach(btn => btn.addEventListener('click', openModal));
+
+function openModal() {
+	modal.classList.add('show');
+}
+function closeModal() {
+	modal.classList.remove('show');
+}
